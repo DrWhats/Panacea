f68688dc227a10632aa8ede603c3c22e09476317
Dr_Wat <hudorojckov.leva@gmail.com>
2021-11-05 14:00:59 +0500

    added_scripts

---
+++ Assets/Scripts/Test_vaccine.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_vaccine : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private GameObject currentPatient;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    
+}
